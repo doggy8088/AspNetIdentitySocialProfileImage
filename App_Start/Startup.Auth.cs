73fcd5563013f52ae8268dda2a726acbd93883b9
Jerrie Pelser <jerrie@jerriepelser.com>
2013-10-28 15:17:58 +0700

    Initial commit

---
+++ App_Start/Startup.Auth.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.Twitter;
+using Owin;
+
+namespace AspNetIdentitySocialProfileImage
+{
+    public partial class Startup
+    {
+        public const string TwitterConsumerKey = "your consumer key here";
+        public const string TwitterConsumerSecret = "your consumer secret here";
+
+        public const string TwitterAccessTokenClaimType = "urn:tokens:twitter:accesstoken";
+        public const string TwitterAccessTokenSecretClaimType = "urn:tokens:twitter:accesstokensecret";
+
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseTwitterAuthentication(
+                new TwitterAuthenticationOptions()
+                {
+                    ConsumerKey = TwitterConsumerKey,
+                    ConsumerSecret = TwitterConsumerSecret,
+                    Provider = new TwitterAuthenticationProvider()
+                    {
+                        OnAuthenticated = async context =>
+                        {
+                            context.Identity.AddClaim(new Claim(TwitterAccessTokenClaimType, context.AccessToken));
+                            context.Identity.AddClaim(new Claim(TwitterAccessTokenSecretClaimType, context.AccessTokenSecret));
+                        }
+                    }
+                }
+               );
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using TweetSharp;
+
+namespace AspNetIdentitySocialProfileImage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (Request.IsAuthenticated)
+            {
+                var claimsIdentity = HttpContext.User.Identity as ClaimsIdentity;
+                var claims = claimsIdentity.Claims;
+
+                var accessTokenClaim = claims.FirstOrDefault(x => x.Type == Startup.TwitterAccessTokenClaimType);
+                var accessTokenSecretClaim =
+                    claims.FirstOrDefault(x => x.Type == Startup.TwitterAccessTokenSecretClaimType);
+
+                if (accessTokenClaim != null && accessTokenSecretClaim != null)
+                {
+                    var service = new TwitterService(
+                        Startup.TwitterConsumerKey,
+                        Startup.TwitterConsumerSecret,
+                        accessTokenClaim.Value,
+                        accessTokenSecretClaim.Value
+                        );
+
+                    ViewBag.Tweets = service.ListTweetsOnHomeTimeline(new ListTweetsOnHomeTimelineOptions());
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
